5de93bed9eeda92c87b08acf9727ae326cae3139
somaia-khalil <sumaya_khair@live.com>
2020-01-28 09:19:27 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(drawves)
+drawves.each_with_index do |drawev, i|
+  puts "#{i+1} #{drawev}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect do |e|
+       e.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(elemnts)
+  elemnts.any? do |w|
+    w.length > 4 
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arrayoffood)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arrayoffood.find do |chees|
+    cheese_types.include?(chees) 
+  end
 end
